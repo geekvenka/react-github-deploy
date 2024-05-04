3173f01136d6ba43ed906af3d9ba8fe1a89f77c5
venky <geekvenka2gmail.com1>
2024-05-04 21:02:39 +0530

    new repository created from vs code IDE for Vercel

---
+++ react-github-deploy/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Vercel Deployment <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React App  in Vercel 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
